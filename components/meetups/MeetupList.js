ac1f66394f18ac49c52dca7c114b68a5e608e0c4
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-31 22:03:39 -0500

    Added useState and useEffect to root index. Passed state as props for MeetupList component. Implemented conditional rendering for meetups in MeetupList component.

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,20 +1,22 @@
-import MeetupItem from './MeetupItem';
-import classes from './MeetupList.module.css';
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
 
-function MeetupList(props) {
+const MeetupList = (props) => {
+  if (!props.meetups) {
+    return <p>Loading...</p>; // Add a loading state when meetups is undefined
+  }
+
+  if (props.meetups.length === 0) {
+    return <p>No meetups found.</p>; // Handle the case when meetups is an empty array
+  }
+
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
+        <MeetupItem key={meetup.id} id={meetup.id} />
       ))}
     </ul>
   );
-}
+};
 
 export default MeetupList;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,37 @@
 // our-domain.com/new-meetup
 
 import MeetupList from "@/components/meetups/MeetupList";
+import { useEffect, useState } from "react";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/6/2016/07/slimer.jpg",
+    address: "Some address.",
+    description: "This is a meetup.",
+  },
+  {
+    id: "m2",
+    title: "Another Meetup",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/6/2016/07/slimer.jpg",
+    address: "Some address.",
+    description: "This is a meetup.",
+  },
+];
 
 function HomePage() {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First Meetup",
-      image:
-        "https://static.onecms.io/wp-content/uploads/sites/6/2016/07/slimer.jpg",
-      address: "Some address.",
-      description: "This is a meetup.",
-    },
-    {
-      id: "m2",
-      title: "Another Meetup",
-      image:
-        "https://static.onecms.io/wp-content/uploads/sites/6/2016/07/slimer.jpg",
-      address: "Some address.",
-      description: "This is a meetup.",
-    },
-  ];
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+
+  const [loadedMeetups, setLoadedMeetups] = useState();
+
+  useEffect(() => {
+    // send a http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS)
+  }, [])
+
+  return <MeetupList meetups={loadedMeetups} />
 
 }
 
