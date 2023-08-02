0be2a06dca8d76ec3905b144a6048d19586b5793
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-01 21:08:12 -0500

    Create .env.local file for mongodb username and mongodb password. Create environmental variables for mongodb username and password for new-meetup.js. Create mongodb connection in new-meetup.js. Create try catch block for error handling.

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,39 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
-if (req.method === "POST") {
-  const data = req.body;
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
 
-  const { title, image, address, description } = data
+    const { title, image, address, description } = data;
 
-}
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+
+    try {
+      const client = await MongoClient.connect(
+        "mongodb+srv://${username}:${password}@cluster0.nfyp4en.mongodb.net/meetups?retryWrites=true&w=majority"
+      );
+
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: "Meetup inserted!" });
+    } catch (error) {
+      console.error("Error occurred:", error);
+      res
+        .status(500)
+        .json({ message: "An error occurred while inserting the data." });
+    }
+  }
 }
 export default handler;
